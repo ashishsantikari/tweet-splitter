9a1bdf95a7800ba669cc4c437a2a8b1e4df8ccb5
Ashish Santikari <mailto.ashishsan#gmail.com>
2018-04-24 23:46:46 +0530

    Added documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+/**
+	* @author Ashish Santikari
+	* @description App Component's controller
+	*/
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,11 +13,20 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-	TWEET_SIZE: number = 50;
+	/**
+		* @name TWEET_SIZE
+		* @description Length of each splitted string.
+		*/
+	private TWEET_SIZE: number = 50;
+
+	//error string
 	public error: String;
+
+	//list of splitted tweets
 	public results: String[] = [];
 
-	submitTweet(tweet_string: String): void {
+	//function called on click event from Tweet Button
+	public submitTweet(tweet_string: String): void {
 		this.error = undefined;
 		try {
 			this.results = this.splitString(tweet_string);
@@ -23,7 +37,13 @@ export class AppComponent {
 		}
 	}
 
-	splitString(str: String): String[] {
+	/**
+		* @name splitString
+		* @param {String} str Input string
+		* @returns Array of splitted input string
+		*/
+	private splitString(str: String): String[] {
+		//if input string length is less than equal to 50, return in an array
 		if (str.length <= 50)
 			return [str];
 
@@ -31,9 +51,12 @@ export class AppComponent {
 		let content_length: number = str.length;
 		let no_of_indicators: number = Math.ceil(content_length / this.TWEET_SIZE);
 		let indicator_max_size: number = (no_of_indicators.toString().length * 2 + 2);
+
+		//actual content length caluclation and total number of required indicators
 		let total_content_length: number = content_length + (no_of_indicators * indicator_max_size);
 		let total_no_of_indicators: number = Math.ceil(total_content_length / this.TWEET_SIZE);
 
+		//variables to keep track of the string creation
 		let split_start: number = 0;
 		let split_end: number;
 		let track_indicator: number = 1;
@@ -49,7 +72,7 @@ export class AppComponent {
 				let tweet_text = str.substring(split_start, split_point);
 				let split_end = tweet_text.lastIndexOf(' ');
 				if (split_end === -1) {
-					throw new Error('MESSGAE_NOT_SPLITTED');
+					throw new Error('MESSGAE_NOT_SPLITTED'); //if no whitespace found, throw error
 				}
 				response.push(indicator_text + str.substring(split_start, split_end));
 				split_start = split_end + 1;
@@ -62,6 +85,12 @@ export class AppComponent {
 		return response;
 	}
 
+	/**
+		* @name getPageIndicator
+		* @description generates the indicator to be prepended before tweet
+		* @param partNum Part number
+		* @param totalIndicator Total number of parts
+		*/
 	private getPageIndicator(partNum: number, totalIndicator: number) {
 		return `${partNum}/${totalIndicator} `;
 	}
